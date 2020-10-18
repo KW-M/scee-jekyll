3f90ae906b5b301229591617e45085935553fca3
Kyle W-M <25kylworc@gmail.com>
2020-10-18 15:20:44 -0700

    Carousel Update + Minor fixes

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,10 +1,23 @@
-var glide = new Glide(".glide", {
-  type: "carousel",
-  startAt: 0,
-  perView: 1,
-  gap:0,
-  focusAt:'center',
-  autoplay: 3000
-});
+// document.addEventListener("onload", function () {
+  homeImageCarousel = document.querySelector(".glide-home");
+  console.log(homeImageCarousel)
+  if (homeImageCarousel) new Glide(homeImageCarousel, {
+    type: "carousel",
+    startAt: 0,
+    perView: 1,
+    gap: 0,
+    focusAt: 'center',
+    autoplay: 4000
+  }).mount()
 
-glide.mount()
+  cardCarousel = document.querySelector(".glide-cards");
+  if (cardCarousel) new Glide(cardCarousel, {
+    type: "carousel",
+    startAt: 0,
+    perView: 4,
+    slideWidth: 300,
+    peek:20,
+    focusAt: 'center',
+    autoplay: 5000
+  }).mount()
+// })
