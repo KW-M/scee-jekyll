d0ef2f77e1b97fa151c9859152a0ccd12521c98d
Kyle W-M <25kylworc@gmail.com>
2020-10-20 15:44:20 -0700

    Improve Carousel Design

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -8,15 +8,27 @@
     gap: 0,
     focusAt: 'center',
     autoplay: 4000
-  }).mount()
+  }).mount();
+
+  // a = Breakpoints.match({
+  //   600: { perView: 1 },
+  //   1200: { perView: 3 }
+  // })
 
   cardCarousel = document.querySelectorAll(".glide-cards");
   cardCarousel.forEach(element => {
     new Glide(element, {
       type: "carousel",
       startAt: 0,
-      perView: 4,
-      slideWidth: 300,
+      breakpoints:{
+        600: { perView: 1 },
+        900: { perView: 2 },
+        1200: { perView: 3 },
+        1600: { perView: 4 },
+        2000: { perView: 5 }
+      },
+      perView:6,
+      gap:20,
       peek:20,
       focusAt: 'center',
       autoplay: 5000
